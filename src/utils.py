4f1c9cabeba9ce940c84ad8141072d5a837b0fae
keeekooo077-afk <keeekooo077@gmail.com>
2026-03-20 01:43:51 +0530

    Done

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import os
-import pickle
-from src.exception import CustomException
 import sys
+import pickle
+import logging
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
@@ -13,27 +16,43 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-from sklearn.metrics import r2_score
-from src.exception import CustomException
-import sys
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        logging.info('Exception occurred in load_object function utils')
+        raise CustomException(e, sys)
+
 
 def evaluate_model(X_train, y_train, X_test, y_test, models):
     try:
         report = {}
 
         for model_name, model in models.items():
-            # Train model
-            model.fit(X_train, y_train)
+            try:
+                # Train
+                model.fit(X_train, y_train)
 
-            # Predictions
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+                # Predict
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
-            # Score
-            train_score = r2_score(y_train, y_train_pred)
-            test_score = r2_score(y_test, y_test_pred)
+                # Scores
+                train_score = r2_score(y_train, y_train_pred)
+                test_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_score
+                report[model_name] = {
+                    "train_score": train_score,
+                    "test_score": test_score
+                }
+
+            except Exception as model_error:
+                logging.warning(f"{model_name} failed: {model_error}")
+                report[model_name] = None
 
         return report
 
